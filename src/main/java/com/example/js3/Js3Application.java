877e5b731e0c35047a0a95da45527f8c7b80cd2b
jpwiig <jpwiig@gmail.com>
2022-01-31 15:03:30 +0100

    1

---
+++ src/main/java/com/example/js3/Js3Application.java
@@ -0,0 +1,13 @@
+package com.example.js3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Js3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Js3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/js3/Js3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.js3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Js3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
